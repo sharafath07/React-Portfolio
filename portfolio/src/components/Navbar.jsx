93b1822959d97e1412124b6a1d7fdf70b67d25a7
Sharafath Ahammed V <142240872+sharafath07@users.noreply.github.com>
2026-04-14 04:14:40 +0000

    make navbar responsive

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -1,34 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { ArrowUpRight, Menu, X } from 'lucide-react';
 import profile from '../assets/profile.svg'
 
+
 function Navbar() {
+    const [isOpen, setIsOpen] = useState(false)
+    const screenWidth = window.innerWidth;
 
     return (
-        <div className='h-screen audiowide bg-black absolute top-0 left-0 right-0 w-screen flex  justify-center'>
-            <div className='flex items-center justify-between space-x-4 w-11/12 h-18 md:h-25 p-3 bg-gray-900 text-white rounded-full m-5 border-2 border-blue-500'>
-                <div className='p-1 flex items-center '>
-                    <img className='mb-3 p-2 w-13 md:w-18' src={profile} alt="img" />
-                    <a href="#" className='text-xl md:text-4xl font-medium hover:text-blue-500 transition duration-300'>Sharafath</a>
-                </div>
-                {
-                    window.innerWidth > 640 ? (
-                        <div className="flex space-x-3 md:space-x-6 text-sm md:text-lg font-medium pr-5 md:pr-10">
-                            <a href="#" className='relative cursor-pointer text-gray-300 hover:text-blue-500 transition duration-300 after:absolute after:left-0 after:-bottom-1 after:h-[2px] after:w-0 after:bg-blue-500 hover:after:w-full after:transition-all after:duration-300'>Home</a>
-                            <a href="#about" className='relative cursor-pointer text-gray-300 hover:text-blue-500 transition duration-300 after:absolute after:left-0 after:-bottom-1 after:h-[2px] after:w-0 after:bg-blue-500 hover:after:w-full after:transition-all after:duration-300'>About</a>
-                            <a href="#projects" className='relative cursor-pointer text-gray-300 hover:text-blue-500 transition duration-300 after:absolute after:left-0 after:-bottom-1 after:h-[2px] after:w-0 after:bg-blue-500 hover:after:w-full after:transition-all after:duration-300'>Projects</a>
-                            <a href="#contact" className='relative cursor-pointer text-gray-300 hover:text-blue-500 transition duration-300 after:absolute after:left-0 after:-bottom-1 after:h-[2px] after:w-0 after:bg-blue-500 hover:after:w-full after:transition-all after:duration-300'>Contact</a>
+        <nav className='bg-gray-800 audiowide rounded-full w-19/20 mx-auto mt-4 pr-4 py-2 outline-3 outline-offset-2 outline-solid outline-[#cc2bfb]'>
+            {
+                screenWidth > 640 ? (
+                    <div className="h-12 md:h-16 items-center flex justify-between">
+                        <a className='flex items-center justify-start cursor-pointer' href="#">
+                            <img src={profile} alt="" className="w-15 md:w-19 relative -top-2 left-1" />
+                            <h1 className='text-xl md:text-3xl text-[#cc2bfb] pl-2 font-medium' href="#">SHARAFATH</h1>
+                        </a>
+                        <div className='hidden sm:block font-medium'>
+                            <a className='relative text-gray-400 text-base md:text-lg mx-2 cursor-pointer hover:text-white transition duration-300 after:absolute after:left-0 after:-bottom-1 after:h-[2px] after:w-0 after:bg-white hover:after:w-full after:transition-all after:duration-300 active:text-[#cc2bfb]' href="#home">HOME</a>
+                            <a className='relative text-gray-400 text-base md:text-lg mx-2 cursor-pointer hover:text-white transition duration-300 after:absolute after:left-0 after:-bottom-1 after:h-[2px] after:w-0 after:bg-white hover:after:w-full after:transition-all after:duration-300 active:text-[#cc2bfb]' href="#about">ABOUT</a>
+                            <a className='relative text-gray-400 text-base md:text-lg mx-2 cursor-pointer hover:text-white transition duration-300 after:absolute after:left-0 after:-bottom-1 after:h-[2px] after:w-0 after:bg-white hover:after:w-full after:transition-all after:duration-300 active:text-[#cc2bfb]' href="#projects">PROJECTS</a>
+                            <a className='relative text-gray-400 text-base md:text-lg mx-2 cursor-pointer hover:text-white transition duration-300 after:absolute after:left-0 after:-bottom-1 after:h-[2px] after:w-0 after:bg-white hover:after:w-full after:transition-all after:duration-300 active:text-[#cc2bfb]' href="#contact">CONTACT</a>
                         </div>
-                    ) : (
-                        <div className='flex flex-col fixed top-0 right-0 bottom-0 w-1/3 justify-center items-center space-y-10 bg-gray-900 text-white text-lg font-medium z-10'>
-                            <a href="#" className='relative cursor-pointer text-gray-300 hover:text-blue-500 transition duration-300 after:absolute after:left-0 after:-bottom-1 after:h-[2px] after:w-0 after:bg-blue-500 hover:after:w-full after:transition-all after:duration-300'>Home</a>
-                            <a href="#about" className='relative cursor-pointer text-gray-300 hover:text-blue-500 transition duration-300 after:absolute after:left-0 after:-bottom-1 after:h-[2px] after:w-0 after:bg-blue-500 hover:after:w-full after:transition-all after:duration-300'>About</a>
-                            <a href="#projects" className='relative cursor-pointer text-gray-300 hover:text-blue-500 transition duration-300 after:absolute after:left-0 after:-bottom-1 after:h-[2px] after:w-0 after:bg-blue-500 hover:after:w-full after:transition-all after:duration-300'>Projects</a>
-                            <a href="#contact" className='relative cursor-pointer text-gray-300 hover:text-blue-500 transition duration-300 after:absolute after:left-0 after:-bottom-1 after:h-[2px] after:w-0 after:bg-blue-500 hover:after:w-full after:transition-all after:duration-300'>Contact</a>
+                    </div>
+                ) : (
+                    <div>
+                        <div className="h-12 items-center flex justify-between" >
+                            <a className='flex items-center justify-start cursor-pointer' href="#">
+                                <img src={profile} alt="" className="w-15 md:w-19 relative -top-2 left-1 " />
+                                <h1 className='text-2xl md:text-3xl text-[#cc2bfb] pl-2 font-medium' href="#">SHARAFATH</h1>
+                            </a>
+                            <div className='px-4 text-3xl text-white block sm:hidden'>
+                                <Menu onClick={() => setIsOpen(!isOpen)} />
+                            </div>
                         </div>
-                    )
-                }
-            </div>
-        </div>
+                        <div>
+                            <X onClick={() => setIsOpen(!isOpen)} className={`${isOpen ? "block" : "hidden"} fixed top-10 right-10 z-99 font-bold text-white`} />
+                            <div className={`${isOpen ? "block" : "hidden"} block sm:hidden bg-gray-800 border-2 border-[#cc2bfb] space-y-8 pb-3 absolute top-0 right-0 h-full w-2/3 flex flex-col items-start justify-center space-y-4 px-10`}>
+                                <hr className="text-[#cc2bfb] w-full h-1 border-2" />
+                                <a className='text-gray-100 text-4xl text-wrap' href="#home">Home</a>
+                                <hr className="text-[#cc2bfb] w-full h-1 border-2" />
+                                <a className='text-gray-100 text-4xl text-wrap' href="#about">About</a>
+                                <hr className="text-[#cc2bfb] w-full h-1 border-2" />
+                                <a className='text-gray-100 text-4xl text-wrap' href="#projects">Projects</a>
+                                <hr className="text-[#cc2bfb] w-full h-1 border-2" />
+                                <a className='text-gray-100 text-4xl text-wrap' href="#contact">Contact</a>
+                                <hr className="text-[#cc2bfb] w-full h-1 border-2" />
+
+                            </div>
+                        </div>
+                    </div>
+
+                )
+            }
+        </nav >
     )
 }
 
--- portfolio/src/components/Page.jsx
+++ portfolio/src/components/Page.jsx
@@ -1,16 +1,11 @@
 import React from 'react'
 import Navbar from './Navbar'
-import { Routes, Route } from "react-router"
 
 
 function Page() {
     return (
         <div>
             <Navbar />
-            {/* <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-            </Routes> */}
         </div>
     )
 }
